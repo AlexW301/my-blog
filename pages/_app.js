eafac36a2333ea89d47dc97e2171795ccbea965a
Alex <alexwaller301@gmail.com>
2022-02-08 08:28:21 -0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import { orange } from '@mui/material/colors';
+
+const theme = createTheme({
+  status: {
+    danger: orange[500],
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
